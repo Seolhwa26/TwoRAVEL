f1cf9758ef444c4acb3a1dd66d4d769d15f4e510
SeolHwa Kim <seolhwa1126@naver.com>
2023-10-04 21:08:38 +0900

    Add files via upload

--- TwoRavel/src/main/java/com/test/tworavel/member/JoinOk.java
+++ TwoRavel/src/main/java/com/test/tworavel/member/JoinOk.java
@@ -29,6 +29,7 @@ public class JoinOk extends HttpServlet {
       //2. 
       String id = "";
       String pw = "";
+      String pw2 = "";
       String mname = "";
       String gender = "";
       String mtel = "";
@@ -44,6 +45,7 @@ public class JoinOk extends HttpServlet {
       
       id = req.getParameter("id");
       pw = req.getParameter("pw");
+      pw2 = req.getParameter("pw2");
       mname = req.getParameter("mname");
       gender = req.getParameter("gender");
       mtel = req.getParameter("mtel");
@@ -53,44 +55,76 @@ public class JoinOk extends HttpServlet {
       email = req.getParameter("email");
       mbti = req.getParameter("mbti");
 
-      
-      //4.
-      MemberDTO dto = new MemberDTO();
-      
-      dto.setId(id);
-      dto.setPw(pw);
-      dto.setMname(mname);
-      dto.setGender(gender);
-      dto.setMtel(mtel);
-      dto.setMptel(mptel);
-      dto.setJumin(jumin);
-      dto.setMaddress(maddress);
-      dto.setEmail(email);
-      dto.setMbti(mbti);
-      
-      System.out.println(dto);
-      
       MemberDAO dao = new MemberDAO();
-      
-      //4 + 5.
-      result = dao.joinMember(dto);
-      
-      
-      if (result == 1) {
-         RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/member/joinok.jsp");
-         dispatcher.forward(req, resp);
-      } else {
-         
-         PrintWriter writer = resp.getWriter();
-         writer.print("<script>");
-         writer.print("alert('failed');");
-         writer.print("history.back();");
-         writer.print("</script>");
-         writer.close();
-         
-      }
-      
-      
+		
+	  int idcheck = dao.checkid(id);
+	  
+	  if (idcheck != 0) {
+		  
+		  req.setCharacterEncoding("UTF-8");
+		  resp.setContentType("text/html; charset=UTF-8");
+    	  
+    	  PrintWriter writer = resp.getWriter();
+          writer.print("<script>");
+          writer.print("alert('아이디가 중복입니다.');");
+          writer.print("history.back();");
+          writer.print("</script>");
+          writer.close();
+		  
+	  } else {
+		  
+		  if (!pw.equals(pw2)) {
+	    	  
+	    	  req.setCharacterEncoding("UTF-8");
+			  resp.setContentType("text/html; charset=UTF-8");
+	    	  
+	    	  PrintWriter writer = resp.getWriter();
+	          writer.print("<script>");
+	          writer.print("alert('비밀번호가 일치하지 않습니다.');");
+	          writer.print("history.back();");
+	          writer.print("</script>");
+	          writer.close();
+	    	  
+	      } else {
+	    	  
+	    	//4.
+	          MemberDTO dto = new MemberDTO();
+	          
+	          dto.setId(id);
+	          dto.setPw(pw);
+	          dto.setPw2(pw2);
+	          dto.setMname(mname);
+	          dto.setGender(gender);
+	          dto.setMtel(mtel);
+	          dto.setMptel(mptel);
+	          dto.setJumin(jumin);
+	          dto.setMaddress(maddress);
+	          dto.setEmail(email);
+	          dto.setMbti(mbti);
+	          
+	          System.out.println(dto);
+	          
+	          //4 + 5.
+	          result = dao.joinMember(dto);
+	          
+	          
+	          if (result == 1) {
+	             RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/member/joinok.jsp");
+	             dispatcher.forward(req, resp);
+	          } else {
+	             
+	             PrintWriter writer = resp.getWriter();
+	             writer.print("<script>");
+	             writer.print("alert('failed');");
+	             writer.print("history.back();");
+	             writer.print("</script>");
+	             writer.close();
+	             
+	          }
+	    	  
+	      }
+		  
+	  }
 
    }
 
