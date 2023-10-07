05756992aafa8f0f56da52b2d62287e3ab21e299
SeolHwa Kim <seolhwa1126@naver.com>
2023-10-08 02:39:19 +0900

    Add files via upload

--- TwoRavel/src/main/java/com/test/tworavel/member/Deljjim.java
+++ TwoRavel/src/main/java/com/test/tworavel/member/Deljjim.java
@@ -13,41 +13,40 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/member/deljjim.do")
 public class Deljjim extends HttpServlet {
 
-   @Override
-   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-	   HttpSession session = req.getSession();
-       //찜 목록 삭제
-       String sseq = req.getParameter("sseq");
-       System.out.println("sseq = " + sseq);
-       String mseq = (String)session.getAttribute("mseq");
-         
-       MPlanDAO dao3 = new MPlanDAO();
-         
-         
-       int result = dao3.deljjim(sseq, mseq);
-       System.out.println(result);
-       if (result == 1) {
-    	
-    	   req.setCharacterEncoding("UTF-8");
-		   resp.setContentType("text/html; charset=UTF-8");
-				
-		   PrintWriter writer = resp.getWriter();
-		   writer.print("<script>");
-		   writer.print("alert('찜 목록에서 삭제 되었습니다.');");
-		   writer.print("window.location.href = '/tworavel/member/mypage.do';");
-		   writer.print("</script>");
-           writer.close(); 
-            
-       } else {
-          PrintWriter writer = resp.getWriter();
-          writer.print("<script>");
-          writer.print("alert('failed');");
-          writer.print("history.back();");
-          writer.print("</script>");
-          writer.close();  
-       }
+		HttpSession session = req.getSession();
+		// 찜 목록 삭제
+		String sseq = req.getParameter("sseq");
+		System.out.println("sseq = " + sseq);
+		String mseq = (String) session.getAttribute("mseq");
 
-   }
+		MPlanDAO dao3 = new MPlanDAO();
+
+		int result = dao3.deljjim(sseq, mseq);
+		System.out.println(result);
+		if (result == 1) {
+
+			req.setCharacterEncoding("UTF-8");
+			resp.setContentType("text/html; charset=UTF-8");
+
+			PrintWriter writer = resp.getWriter();
+			writer.print("<script>");
+			writer.print("alert('찜 목록에서 삭제 되었습니다.');");
+			writer.print("window.location.href = '/tworavel/member/mypage.do';");
+			writer.print("</script>");
+			writer.close();
+
+		} else {
+			PrintWriter writer = resp.getWriter();
+			writer.print("<script>");
+			writer.print("alert('failed');");
+			writer.print("history.back();");
+			writer.print("</script>");
+			writer.close();
+		}
+
+	}
 
 }
